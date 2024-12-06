64f71fa192aa08386cde9c15d01dfd74cb39cd24
andsin09 <161988024+andsin09@users.noreply.github.com>
2024-12-06 09:16:17 -0500

    Update asdfsd.cpp

--- asdfsd.cpp
+++ asdfsd.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
 
+int main(){
+    string names;
+    cout << "Enter you name, friend." << endl;
+    cin >> names;
+    for(int i = 0; i < names.length(); i++){
+        if(isdigit(names[i]) || !isalnum(names[i])){
+            cout << "No digits please.";
+            return 0;
+        }
+        else{
+            putchar(toupper(names[i]));
+        }
+    }
+}
